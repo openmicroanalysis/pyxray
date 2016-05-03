338dbd03bd588c7b405b2a69a147525c1dd83252
Philippe Pinard <philippe.pinard@gmail.com>
2016-05-03 17:26:19 +0200

    Use versioneer.

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@
 from setuptools import setup
 
 # Local modules.
+import versioneer
 
 # Globals and constants variables.
 
 setup(name="pyxray",
-      version='0.1',
+      version=versioneer.get_version(),
       url='http://pyxray.bitbucket.org',
-      description="Definitions and properties of x-ray transitions",
+      description="Definitions and properties of X-ray transitions",
       author="Hendrix Demers and Philippe T. Pinard",
       author_email="hendrix.demers@mail.mcgill.ca and philippe.pinard@gmail.com",
       license="MIT",
@@ -31,5 +32,7 @@ setup(name="pyxray",
       install_requires=['pyparsing'],
 
       test_suite='pyxray.tests.test_all',
+
+      cmdclass=versioneer.get_cmdclass(),
 )
 
