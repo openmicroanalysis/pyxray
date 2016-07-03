3947bb8b3bb429b0c3f9c5ece5523b8af3e14bbd
Philippe Pinard <philippe.pinard@gmail.com>
2016-07-03 16:43:59 +0100

    Add doc.

--- pyxray/sql/data.py
+++ pyxray/sql/data.py
@@ -20,6 +20,15 @@ class _SqlEngineDatabase(_Database):
         self.engine = engine
 
     def _get(self, queried_columns, filters, exception, reference=None):
+        """
+        Returns values from the database based on the specified columns, 
+        filters and reference.
+        If no result is found, the exception is raised.
+        This method respects the reference priority defined in the database
+        if no reference is specified.
+        
+        :return: :class:`list` of length equal to the number of *queried_columns*
+        """
         queried_columns.append(Reference.bibtexkey)
 
         with session_scope(self.engine) as session:
@@ -30,17 +39,22 @@ class _SqlEngineDatabase(_Database):
             q = q.order_by(Reference.id)
 
             results = OrderedDict((k, v) for * v, k in q.all())
+
+            # No results from query
             if not results:
                 raise exception
 
+            # Check result from specified reference
             if reference is not None:
                 if reference in results:
                     return results[reference]
                 else:
                     raise exception
 
+            # Check result from preferred references
             for reference in self.reference_priority:
                 if reference in results:
                     return results[reference]
 
+            # Return first result
             return results.popitem(last=False)[1]
