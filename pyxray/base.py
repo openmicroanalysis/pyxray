1a5012bbcc2f493549881f16fb6900226f3baf38
Philippe Pinard <philippe.pinard@gmail.com>
2016-10-25 07:37:34 +0100

    Start of base database implementation.

--- pyxray/base.py
+++ pyxray/base.py
@@ -6,14 +6,13 @@ Base database
 import abc
 
 # Third party modules.
-import six
 
 # Local modules.
+from pyxray.descriptor import Element
 
 # Globals and constants variables.
 
-@six.add_metaclass(abc.ABCMeta)
-class _Database(object):
+class _Database(object, metaclass=abc.ABCMeta):
 
     def __init__(self):
         self.reference_priority = []
@@ -36,28 +35,8 @@ class _Database(object):
         """
         self._reference_priority = list(references)
 
-    def _get_z(self, zeq):
-        """
-        Returns the atomic number.
-        
-        :arg zeq: atomic number equivalent, accepts atomic number, symbol,
-            object with :attr:`z` or :attr:`atomic_number`
-            
-        :rtype: :class:`int`
-        """
-        if isinstance(zeq, int) and zeq >= 1 and zeq <= 100:
-            return zeq
-        elif isinstance(zeq, six.string_types):
-            return self.element_atomic_number(zeq)
-        elif hasattr(zeq, 'z'):
-            return zeq.z
-        elif hasattr(zeq, 'atomic_number'):
-            return zeq.atomic_number
-        else:
-            raise ValueError('Unknown object: {0}'.format(zeq))
-
     @abc.abstractmethod
-    def element_symbol(self, z):
+    def element_symbol(self, element, reference=None):
         """
         Returns the element's symbol.
 
@@ -68,7 +47,7 @@ class _Database(object):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def element_atomic_number(self, symbol):
+    def element_atomic_number(self, element, reference=None):
         """
         Returns the atomic number for the specified symbol.
         This function is case insensitive.
@@ -80,7 +59,7 @@ class _Database(object):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def element_name(self, zeq, language='en', reference=None):
+    def element_name(self, element, language='en', reference=None):
         """
         Returns the full name of an element, in the language specified, and
         its reference.
@@ -97,7 +76,7 @@ class _Database(object):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def element_atomic_weight(self, zeq, reference=None):
+    def element_atomic_weight(self, element, reference=None):
         """
         Returns the atomic weight and its reference.
         The atomic weight is defined by the CIAAW as it is the ratio of 
@@ -116,7 +95,7 @@ class _Database(object):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def element_mass_density_kg_per_m3(self, zeq, reference=None):
+    def element_mass_density_kg_per_m3(self, element, reference=None):
         """
         Returns the mass density (in kg/m3) and its reference.
 
@@ -131,7 +110,7 @@ class _Database(object):
         """
         raise NotImplementedError
 
-    def element_mass_density_g_per_cm3(self, zeq, reference=None):
+    def element_mass_density_g_per_cm3(self, element, reference=None):
         """
         Returns the mass density (in g/cm3) and its reference.
 
@@ -144,5 +123,5 @@ class _Database(object):
             
         :return: mass density (in g/cm3) and reference key
         """
-        return self.element_mass_density_kg_per_m3(zeq, reference) / 1e3
+        return self.element_mass_density_kg_per_m3(element, reference) / 1e3
 
