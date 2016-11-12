8440546df1dd1a62cd32b2d6640eb876747e1828
Philippe Pinard <philippe.pinard@gmail.com>
2016-11-12 13:49:52 +0000

    Add some unit tests

---
+++ pyxray/parser/test_jenkins1991.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+""" """
+
+# Standard library modules.
+import unittest
+import logging
+
+# Third party modules.
+
+# Local modules.
+from pyxray.parser.jenkins1991 import Jenkins1991TransitionNotationParser
+
+# Globals and constants variables.
+
+class TestJenkins1991TransitionNotationParser(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        self.parser = Jenkins1991TransitionNotationParser()
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test__iter__(self):
+        props = list(self.parser)
+        self.assertEqual(41, len(props))
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
---
+++ pyxray/parser/test_perkins1991.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+""" """
+
+# Standard library modules.
+import unittest
+import logging
+
+# Third party modules.
+
+# Local modules.
+from pyxray.parser.perkins1991 import Perkins1991Parser
+# Globals and constants variables.
+
+class TestJenkins1991TransitionNotationParser(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        self.parser = Perkins1991Parser()
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test__iter__(self):
+        props = list(self.parser)
+        self.assertEqual(19189, len(props))
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
