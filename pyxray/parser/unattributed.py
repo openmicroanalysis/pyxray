e353b98f7fee9d63fcdc53a0a5942f0ea94b1ccd
Philippe Pinard <philippe.pinard@gmail.com>
2016-11-12 13:32:48 +0000

    Add parser for IUPAC transition notation.

--- pyxray/parser/unattributed.py
+++ pyxray/parser/unattributed.py
@@ -10,8 +10,10 @@ logger = logging.getLogger(__name__)
 
 # Local modules.
 from pyxray.parser.parser import _Parser
-from pyxray.descriptor import Reference, Element, AtomicShell, Notation, AtomicSubshell
-from pyxray.property import ElementSymbol, AtomicShellNotation, AtomicSubshellNotation
+from pyxray.descriptor import \
+    Reference, Element, AtomicShell, Notation, AtomicSubshell, Transition
+from pyxray.property import \
+    ElementSymbol, AtomicShellNotation, AtomicSubshellNotation, TransitionNotation
 
 # Globals and constants variables.
 
@@ -20,11 +22,17 @@ UNATTRIBUTED = Reference('unattributed')
 MAX_N = 7
 
 def iter_subshells(max_n):
+    nprev = 0
     for n in range(1, max_n + 1):
+        if nprev != n:
+            i = 1
+
         for l in range(0, n):
             for j in set([abs(l + 0.5), abs(l - 0.5)]):
                 j_n = int(j * 2)
-                yield n, l, j_n
+                yield n, l, j_n, i
+                i += 1
+                nprev = n
 
 class ElementSymbolPropertyParser(_Parser):
 
@@ -81,38 +89,35 @@ class AtomicSubshellNotationParser(_Parser):
                         'IX', 'X', 'XI', 'XII', 'XIII']
     ORBITAL_L = ['s', 'p', 'd', 'f', 'g', 'h', 'i']
 
-    def _create_entry_siegbahn(self, n, l, j_n, i):
-        shell = self.SHELLS['siegbahn'][n - 1]
+    @classmethod
+    def _create_entry_siegbahn(cls, n, l, j_n, i):
+        shell = cls.SHELLS['siegbahn'][n - 1]
         s = shell
-        if i > 1:
-            s += self.SIEGBAHN_NUMBERS[i - 1]
+        if n != 1: # No KI
+            s += cls.SIEGBAHN_NUMBERS[i - 1]
         return s, s, s, s
 
-    def _create_entry_iupac(self, n, l, j_n, i):
-        shell = self.SHELLS['iupac'][n - 1]
+    @classmethod
+    def _create_entry_iupac(cls, n, l, j_n, i):
+        shell = cls.SHELLS['iupac'][n - 1]
         s = html = latex = shell
-        if i > 1:
+        if n != 1: # No K1
             s += str(i)
             html += '<sub>{0:d}</sub>'.format(i)
             latex += '$_{{{0:d}}}$'.format(i)
         return s, s, html, latex
 
-    def _create_entry_orbital(self, n, l, j_n, i):
-        l_letter = self.ORBITAL_L[l]
+    @classmethod
+    def _create_entry_orbital(cls, n, l, j_n, i):
+        l_letter = cls.ORBITAL_L[l]
         s = '{0:d}{1}{2}/2'.format(n, l_letter, j_n)
         html = '{0:d}{1}<sub>{2}/2</sub>'.format(n, l_letter, j_n)
         latex = '{0:d}{1}$_{{{2}/2}}$'.format(n, l_letter, j_n)
         return s, s, html, latex
 
     def __iter__(self):
-        length = MAX_N
-        progress = 0
-        nprev = 0
-        for n, l, j_n in iter_subshells(MAX_N):
-            if nprev != n:
-                i = 1
-                self.update(int(progress / length * 100.0))
-                progress += 1
+        for n, l, j_n, i in iter_subshells(MAX_N):
+            self.update(int(n / MAX_N * 100.0))
 
             atomic_subshell = AtomicSubshell(n, l, j_n)
 
@@ -143,7 +148,49 @@ class AtomicSubshellNotationParser(_Parser):
             logger.debug('Parsed: {0}'.format(prop))
             yield prop
 
-            i += 1
-            nprev = n
+class TransitionNotationParser(_Parser):
 
+    def __iter__(self):
+        length = (MAX_N * MAX_N) ** 2
+        progress = 0
+        for n0, l0, j0_n, i0 in iter_subshells(MAX_N):
+            src = AtomicSubshell(n0, l0, j0_n)
+            ascii0, utf0, html0, latex0 = \
+                AtomicSubshellNotationParser._create_entry_iupac(n0, l0, j0_n, i0)
 
+            for n1, l1, j1_n, i1 in iter_subshells(MAX_N):
+                progress += 1
+                self.update(int(progress / length * 100.0))
+
+                # Cannot transition to itself
+                if n0 == n1 and l0 == l1 and j0_n == j1_n:
+                    continue
+
+                # Transition must be from more to less energytic shell or
+                # within the same shell
+                if n0 < n1:
+                    continue
+
+                # Coster-Kroning transition must be from more to less energetic subshells
+                if n0 == n1 and i0 <= i1:
+                    continue
+
+                dest = AtomicSubshell(n1, l1, j1_n)
+                transition = Transition(src, dest)
+
+                ascii1, utf1, html1, latex1 = \
+                    AtomicSubshellNotationParser._create_entry_iupac(n1, l1, j1_n, i1)
+
+                ascii = '{0}-{1}'.format(ascii1, ascii0)
+                utf = '{0}\u2013{1}'.format(utf1, utf0)
+                html = '{0}&ndash;{1}'.format(html1, html0)
+                latex = '{0}--{1}'.format(latex1, latex0)
+
+                prop = TransitionNotation(UNATTRIBUTED,
+                                          transition,
+                                          Notation('iupac'),
+                                          ascii, utf, html, latex)
+                logger.debug('Parsed: {0}'.format(prop))
+                yield prop
+
+list(TransitionNotationParser())
