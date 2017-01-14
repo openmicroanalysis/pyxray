ab984aefd45334cc1d9825ad94efe5f4b3f40667
Philippe Pinard <philippe.pinard@gmail.com>
2017-01-14 12:53:00 +0000

    Fix bugs
    wheel for py3 only
    missing data in tarball
    missing pyxray.sql in wheel

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ class build_py(_build_py.build_py):
         builder = pyxray.sql.build.SqliteDatabaseBuilder()
         builder.build()
 
+        del self.data_files # Force reinitialization of files to copy
+
         super().run()
 
 with open(os.path.join(BASEDIR, 'README.rst'), 'r') as fp:
@@ -35,18 +37,18 @@ CMDCLASS['build_py'] = build_py
 ENTRY_POINTS = {
     'pyxray.parser':
       [
-      'unattributed element symbol = pyxray.parser.unattributed:ElementSymbolPropertyParser',
-      'unattributed atomic shell notation = pyxray.parser.unattributed:AtomicShellNotationParser',
-      'unattributed atomic subshell notation = pyxray.parser.unattributed:AtomicSubshellNotationParser',
-      'unattributed transition notation = pyxray.parser.unattributed:TransitionNotationParser',
-      'wikipedia element name = pyxray.parser.wikipedia:WikipediaElementNameParser',
-      'sargent-welch element atomic weight = pyxray.parser.sargent_welch:SargentWelchElementAtomicWeightParser',
-      'sargent-welch element mass density = pyxray.parser.sargent_welch:SargentWelchElementMassDensityParser',
-      'jenkins1991 transition notation = pyxray.parser.jenkins1991:Jenkins1991TransitionNotationParser',
-      'perkins1991 = pyxray.parser.perkins1991:Perkins1991Parser',
-      'nist atomic weight = pyxray.parser.nist:NISTElementAtomicWeightParser',
-      'jeol transition = pyxray.parser.jeol:JEOLTransitionParser',
-      'campbell2001 = pyxray.parser.campbell2001:CampbellAtomicSubshellRadiativeWidthParser',
+        'unattributed element symbol = pyxray.parser.unattributed:ElementSymbolPropertyParser',
+        'unattributed atomic shell notation = pyxray.parser.unattributed:AtomicShellNotationParser',
+        'unattributed atomic subshell notation = pyxray.parser.unattributed:AtomicSubshellNotationParser',
+        'unattributed transition notation = pyxray.parser.unattributed:TransitionNotationParser',
+        'wikipedia element name = pyxray.parser.wikipedia:WikipediaElementNameParser',
+        'sargent-welch element atomic weight = pyxray.parser.sargent_welch:SargentWelchElementAtomicWeightParser',
+        'sargent-welch element mass density = pyxray.parser.sargent_welch:SargentWelchElementMassDensityParser',
+        'jenkins1991 transition notation = pyxray.parser.jenkins1991:Jenkins1991TransitionNotationParser',
+        'perkins1991 = pyxray.parser.perkins1991:Perkins1991Parser',
+        'nist atomic weight = pyxray.parser.nist:NISTElementAtomicWeightParser',
+        'jeol transition = pyxray.parser.jeol:JEOLTransitionParser',
+        'campbell2001 = pyxray.parser.campbell2001:CampbellAtomicSubshellRadiativeWidthParser',
        ],
       }
 
